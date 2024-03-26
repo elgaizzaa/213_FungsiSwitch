64b46ef72405fc29a0b7f56d96a89e166a0189e2
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-03-26 14:36:05 +0700

    Add project files.

---
+++ 213_LATIHAN1/213_LATIHAN1.cpp
@@ -0,0 +1,22 @@
+// 213_LATIHAN1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int bilangan1, bilangan2;
+
+
+
+
+int penjumlahan(int a, int b)
+{
+	return a + b;
+}
+
+
+
+
+int main()
+{
+   
